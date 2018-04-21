d77954a4703596236fd31f9796c3c9c659f0ab6b
sorryDave <sorrydave403@gmail.com>
2018-04-21 15:10:50 +0200

    hero added

--- gameSys.py
+++ gameSys.py
@@ -5,4 +5,67 @@ Created on Sat Apr 21 11:41:24 2018
 
 @author: hellodave
 """
+#
+#import datetime # we will use this for date objects
+#
+#class Person:
+#
+#    def __init__(self, name, surname, birthdate, address, telephone, email):
+#        self.name = name
+#        self.surname = surname
+#        self.birthdate = birthdate
+#
+#        self.address = address
+#        self.telephone = telephone
+#        self.email = email
+#
+#    def age(self):
+#        today = datetime.date.today()
+#        age = today.year - self.birthdate.year
+#
+#        if today < datetime.date(today.year, self.birthdate.month, self.birthdate.day):
+#            age -= 1
+#
+#        return age
+#
+#person = Person(
+#    "Jane",
+#    "Doe",
+#    datetime.date(1992, 3, 12), # year, month, day
+#    "No. 12 Short Street, Greenville",
+#    "555 456 0987",
+#    "jane.doe@example.com"
+#)
+#
+#print(person.name)
+#print(person.email)
+#print(person.age())
+#
 
+
+class Human:
+    def __init__(self, name, age, health, strength):
+        self.name=name
+        self.age=age
+        self.health = health
+        self.strength=strength
+        #self.dexterity=dexterity
+        #self.intelligence=intelligence
+        #self.wisdom=wisdom
+        #self.damage = 0
+    
+    def printStats(self):
+        print("printing...stats")
+        print("\nname: {}\nhealth:{}\nage{}\nstrength:{}".format(self.name, self.health, self.age, self.strength))
+    def setHealth(h):
+        pass        
+        
+        
+  
+    
+human1 = Human("gawain", 29, 100, 9)
+
+
+class diceRoll:
+    pass
+    
---
+++ randomSys.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 21 11:41:24 2018
+
+@author: hellodave
+"""
+import random
+#random.sample(range(1, 100), 3)
+#numLow=0
+#numHigh=0
+#
+#listOfNumbers=[]
+#
+#def randum(n):
+#    random.randint(numLow, numHigh)
+#    
+#    for x in range(0, n):
+#        listOfNumbers.append(random.randint(numLow, numHigh))
+        
+#####################################################33
+        
+
+
+def dice(n, nmax):
+    print("Rolling dices...")
+    print("The values are....")
+    print(random.randint(n, nmax))
+    print (random.randint(n,nmax))
+    
+
+
+    
+
+
+
+
+#################################################################
+#question='n'
+#
+#while question != 'n':
+#    if question == 'y':
+#        die1 = random.randint(1, 6)
+#        die2 = random.randint(1, 6)
+#        print(die1, die2)
+#        question = input('Would you like to roll the dice [y/n]?\n')
+#    else:
+#        print('Invalid response. Please type "y" or "n".')
+#        question = input('Would you like to roll the dice [y/n]?\n')
+#
+#        print('Good-bye!')
+#
+#    
